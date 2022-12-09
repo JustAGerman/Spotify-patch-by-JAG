3d81b43ab12796402605292d3ec20e1f6ed1821c
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2022-12-09 22:45:02 +0100

    Delete Spot directory

--- Spot/Install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/JustAGerman/Spotify-patch-by-JAG/main/Spot/Install.ps1').Content | Invoke-Expression}"
-
-pause
-exit /b
