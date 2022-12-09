eec706daade81ca690a85f259acfa9f0d32f6ec9
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2022-12-09 22:37:49 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/JustAGerman/Spotify-patch-by-JAG/main/Spot/Install.ps1').Content | Invoke-Expression}"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/JustAGerman/Spotify-patch-by-JAG/main/Install.ps1').Content | Invoke-Expression}"
 
 pause
 exit /b
