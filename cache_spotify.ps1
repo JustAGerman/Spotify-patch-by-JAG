86668d02a47a2a8c194142e2860c495773dd4366
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2023-02-06 16:51:50 +0100

    Update cache_spotify.ps1

--- cache_spotify.ps1
+++ cache_spotify.ps1
@@ -1,24 +1,13 @@
-<# 
-Name: Clear Spotify Cache.
-
-Description: The script clears outdated cache from the listened music in Spotify.
-Fires every time you completely close the client (If the client was minimized to tray then the script will not work).
-
-For the APPDATA\Spotify\Data folder, the rule is that all cache files that are not used
-by the customer more than the specified number of days will be deleted.
-
-#>
-
-$day = 7 # Number of days after which the cache is considered stale 
+$day = 7 # Number of days after which the cache is considered stale
 
 # Clear the \Data folder if it finds an outdated cache
-
 try {
-    If (!(Test-Path -Path $env:LOCALAPPDATA\Spotify\Data)) {
+    $SpotifyData = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Spotify\Data"
+    If (!(Test-Path -Path $SpotifyData)) {
         "$(Get-Date -Format "dd/MM/yyyy HH:mm:ss") Folder Local\Spotify\Data not found" | Out-File log.txt -append
         exit	
     }
-    $check = Get-ChildItem $env:LOCALAPPDATA\Spotify\Data -File -Recurse | Where-Object lastaccesstime -lt (get-date).AddDays(-$day)
+    $check = Get-ChildItem $SpotifyData -File -Recurse | Where-Object lastaccesstime -lt (get-date).AddDays(-$day)
     if ($check.Length -ge 1) {
 
         $count = $check
@@ -31,7 +20,7 @@ try {
             $mb = "{0:N2} Mb" -f (($check | Measure-Object Length -s).sum / 1Mb)
             "$(Get-Date -Format "dd/MM/yyyy HH:mm:ss") Removed $mb obsolete cache" | Out-File log.txt -append
         }
-        Get-ChildItem $env:LOCALAPPDATA\Spotify\Data -File -Recurse | Where-Object lastaccesstime -lt (get-date).AddDays(-$day) | Remove-Item
+        Get-ChildItem $SpotifyData -File -Recurse | Where-Object lastaccesstime -lt (get-date).AddDays(-$day) | Remove-Item
     }
     if ($check.Length -lt 1) {
         "$(Get-Date -Format "dd/MM/yyyy HH:mm:ss") Stale cache not found" | Out-File log.txt -append
